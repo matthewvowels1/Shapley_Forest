9d207c08d6da198241a3bbd73fa77ecdf31e153b
Matthew Vowels <matthewvowels1@btinternet.com>
2021-03-01 21:12:33 +0000

    added check additivity option

--- RF_Shap.py
+++ RF_Shap.py
@@ -587,7 +587,7 @@ class RFShap(object):
         return self.model
 
 
-    def run_shap_explainer(self, model):
+    def run_shap_explainer(self, model, check_additivity=True):
 
         """
         :param model: give it a trained model
@@ -610,13 +610,13 @@ class RFShap(object):
         # note that the interventioanl option requires a 'background dataset'
 
         if self.class_ == 'RF':
-            explainer = shap.TreeExplainer(model=model, model_output=model_output, feature_perturbation="tree_path_dependent")
+            explainer = shap.TreeExplainer(model=model, model_output=model_output, feature_perturbation="tree_path_dependent", check_additivity=check_additivity)
             shap_vals = explainer.shap_values(X_test)
 
             joblib.dump(explainer, os.path.join(self.output_dir, self.outcome_var + "_tree_explainer.sav"))
             joblib.dump(shap_vals, os.path.join(self.output_dir, self.outcome_var + "_tree_explainer_shap_values.sav"))
         elif self.class_ == 'lin':
-            explainer = shap.LinearExplainer(model, X_test)
+            explainer = shap.LinearExplainer(model, X_test, check_additivity=check_additivity)
             shap_vals = explainer.shap_values(X_test)
             joblib.dump(explainer, os.path.join(self.output_dir, self.outcome_var + '_linear_explainer.sav'))
             joblib.dump(shap_vals, os.path.join(self.output_dir, self.outcome_var + '_linear_explainer_shap_values.sav'))
